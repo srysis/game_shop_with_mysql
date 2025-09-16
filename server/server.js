ac8bbda6b11f84c6319861b4dec453b1666a27cc
srysis <jashandenis@gmail.com>
2025-09-16 10:38:31 +0200

    use env variables to define SQL db connection settings; prepend each API call with '/api/'; import 'dotenv' and 'mysql2' modules; changed port settings in 'app.listen' function

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
 const body_parser = require('body-parser');
+
+const mysql = require('mysql2');
+const dot_env = require('dotenv');
+
+dot_env.config();
 
 const app = express();
 app.use(express());
@@ -9,12 +13,13 @@ app.use(cors());
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: true}));
 
-
+global.port = process.env.PORT;
 
 const database = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	user: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
 	database: 'game_shop_db'
 });
 
@@ -24,7 +29,7 @@ app.get('/', (request, response) => {
 });
 
 // API call to select all games
-app.get('/games', (request, response) => {
+app.get('/api/games', (request, response) => {
 	const sql_query = "SELECT `games`.`id`, `games`.`name`, `games`.`box_art`, `games`.`price`, `tags`.`tags_list`" +
 					  "FROM games INNER JOIN tags ON games.id = tags.id";
 	
@@ -36,7 +41,7 @@ app.get('/games', (request, response) => {
 })
 
 // API call to select a game where it's 'id' matches the 'id' passed in 'params' of 'request'
-app.get('/games/:id', (request, response) => {
+app.get('/api/games/:id', (request, response) => {
 	const requsted_game_id = request.params.id;
 
 	const sql_query = "SELECT `games`.`id`, `games`.`name`, `games`.`box_art`, `games`.`price`, `games`.`short_description`," + 
@@ -54,7 +59,7 @@ app.get('/games/:id', (request, response) => {
 })
 
 // API call to get cart's data
-app.get('/cart', (request, response) => {
+app.get('/api/cart', (request, response) => {
 	const sql_query = "SELECT `cart_content` from `cart` WHERE `cart`.`id` = 1";
 
 	database.query(sql_query, (error, data) => {
@@ -65,7 +70,7 @@ app.get('/cart', (request, response) => {
 })
 
 //API call to save new cart's data in the database
-app.post('/updateCart', (request, response, next) => {
+app.post('/api/updateCart', (request, response, next) => {
 	let received_IDs = request.body;
 
 	const sql_query = "UPDATE `cart` SET `cart_content` = '" + received_IDs.toString() + "' WHERE `id` = 1";
@@ -78,7 +83,7 @@ app.post('/updateCart', (request, response, next) => {
 })
 
 // API call to select any game where it's name matches the string passed in the request
-app.post('/games/search', (request, response, next) => {
+app.post('/api/games/search', (request, response, next) => {
 	let received_string = Object.keys(request.body);
 
 	const sql_query = "SELECT `games`.`id`, `games`.`name`, `games`.`box_art`, `games`.`price` FROM games WHERE name LIKE '%" + received_string + "%'";
@@ -90,4 +95,4 @@ app.post('/games/search', (request, response, next) => {
 	})
 })
 
-app.listen(8081, () => {console.log("server runs on port 8081")});
+app.listen(process.env.PORT || port, () => {console.log(`server runs on port ${port}`)});
